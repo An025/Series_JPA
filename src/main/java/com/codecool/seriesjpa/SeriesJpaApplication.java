c2d97cb95d3614b3f945528a08a9148e61e1e808
Ani <jancso.anita@gmail.com>
2021-09-23 10:31:51 +0200

    Create spring boot project

---
+++ src/main/java/com/codecool/seriesjpa/SeriesJpaApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.seriesjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/seriesjpa/SeriesJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.seriesjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
